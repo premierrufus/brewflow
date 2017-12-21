ebda6c510aec24558e6253801a56c2b55f27e166
Gabriel <banded.gabriel@gmail.com>
2017-12-21 11:54:13 -0500

    registered beer model with admin

--- cellar/admin.py
+++ cellar/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Tank, Barrel
+from .models import Tank, Barrel, Beer
 
 # Register your models here.
 
 admin.site.register(Tank)
 admin.site.register(Barrel)
+admin.site.register(Beer)
--- cellar/models.py
+++ cellar/models.py
@@ -4,6 +4,12 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Beer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Tank(models.Model):
     title = models.CharField(max_length=4)
 
