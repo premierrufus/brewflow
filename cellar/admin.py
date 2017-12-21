bcdb3febf7392012af74d8cea6b1877f1936022f
Gabriel <banded.gabriel@gmail.com>
2017-12-21 11:26:32 -0500

    registered barrel model with admin

--- cellar/admin.py
+++ cellar/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Tank
+from .models import Tank, Barrel
 
 # Register your models here.
 
 admin.site.register(Tank)
+admin.site.register(Barrel)
--- cellar/models.py
+++ cellar/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Tank(models.Model):
     title = models.CharField(max_length=4)
 
@@ -16,3 +17,29 @@ class Tank(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Barrel(models.Model):
+    serial = models.CharField(max_length=8)
+
+    SPIRITS = (
+        ('b', 'Bourbon'),
+        ('bm', 'Bourbon/Maple'),
+        ('g', 'Gin'),
+        ('r', 'Rum'),
+        ('smw', 'Single Malt Whiskey'),
+        ('t', 'Tequila'),
+        ('w', 'Whiskey'),
+    )
+
+    spirit_profile = models.CharField(max_length=4, choices=SPIRITS, blank=True, default='bm')
+
+    BARREL_STATUS = (
+        ('a', 'Available'),
+        ('i', 'In Use'),
+    )
+
+    status = models.CharField(max_length=1, choices=BARREL_STATUS, blank=True, default='a')
+
+    def __str__(self):
+        return self.serial
